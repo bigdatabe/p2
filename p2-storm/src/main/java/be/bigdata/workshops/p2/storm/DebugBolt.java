1777b7fdcf86ed9d2673800a75b34b294858d792
dierickx <dierickx@BE1XPL-100239.(none)>
2013-05-30 21:22:52 +0200

    added debug bolt

---
+++ p2-storm/src/main/java/be/bigdata/workshops/p2/storm/DebugBolt.java
@@ -0,0 +1,44 @@
+package be.bigdata.workshops.p2.storm;
+
+import java.util.Map;
+
+import org.apache.log4j.Logger;
+
+import backtype.storm.task.OutputCollector;
+import backtype.storm.task.TopologyContext;
+import backtype.storm.topology.OutputFieldsDeclarer;
+import backtype.storm.topology.base.BaseRichBolt;
+import backtype.storm.tuple.Tuple;
+
+/**
+ * Simple input logger bolt.
+ * 
+ * @author dierickx
+ */
+public class DebugBolt extends BaseRichBolt {
+
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 1037052250323137715L;
+    private static Logger LOGGER = Logger.getLogger(DebugBolt.class);
+
+    @Override
+    public void prepare(final Map stormConf, final TopologyContext context, final OutputCollector collector) {
+        LOGGER.info("Preparing debug bolt.");
+    }
+
+    @Override
+    public void execute(final Tuple input) {
+        for (final String field : input.getFields()) {
+            LOGGER.info(String.format("%s = '%s'", field, input.getStringByField(field)));
+        }
+    }
+
+    @Override
+    public void declareOutputFields(final OutputFieldsDeclarer declarer) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
