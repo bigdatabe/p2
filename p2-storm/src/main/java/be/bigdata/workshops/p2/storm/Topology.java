ed356d16c829d23ca45a8d555701c843cc4a627e
Geoffrey De Smet <gds.geoffrey.de.smet@gmail.com>
2013-05-30 21:15:44 +0200

    hello world

--- p2-storm/src/main/java/be/bigdata/workshops/p2/storm/Topology.java
+++ p2-storm/src/main/java/be/bigdata/workshops/p2/storm/Topology.java
@@ -1,7 +1,26 @@
 package be.bigdata.workshops.p2.storm;
 
+import backtype.storm.Config;
+import backtype.storm.LocalCluster;
+import backtype.storm.generated.StormTopology;
+import backtype.storm.testing.TestWordSpout;
+import backtype.storm.topology.TopologyBuilder;
+import backtype.storm.utils.Utils;
+
 public class Topology {
+
     public static void main(String[] args) {
+        System.out.println("Team 3");
+        TopologyBuilder builder = new TopologyBuilder();
+        builder.setSpout("nonsenseBolt", new TestWordSpout());
+        builder.setBolt("systemOutBolt", new SystemOutBolt()).shuffleGrouping("nonsenseBolt");
+        StormTopology topology = builder.createTopology();
+        LocalCluster cluster = new LocalCluster();
+        cluster.submitTopology("team3Tolopogy", new Config(), topology);
+        Utils.sleep(10000);
+        cluster.killTopology("test");
+        cluster.shutdown();
 
     }
+
 }
