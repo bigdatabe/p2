f1f21f37c90073ae3049af83a7b175383827e2b9
Olivier Van Laere <oliviervanlaere@gmail.com>
2013-05-30 21:21:59 +0200

    Adding Topology and initial bolt (hashtag summary)
    Signed-off-by: Olivier Van Laere <oliviervanlaere@gmail.com>

--- p2-storm/src/main/java/be/bigdata/workshops/p2/storm/Topology.java
+++ p2-storm/src/main/java/be/bigdata/workshops/p2/storm/Topology.java
@@ -1,7 +1,31 @@
 package be.bigdata.workshops.p2.storm;
 
-public class Topology {
-    public static void main(String[] args) {
+import backtype.storm.Config;
+import backtype.storm.LocalCluster;
+import backtype.storm.topology.TopologyBuilder;
 
-    }
+/**
+ * To run this topology you should execute this main as: 
+ * java -cp XXX.jar twitter.streaming.Topology <track> <twitterUser> <twitterPassword>
+ *
+ * @see https://github.com/storm-book/examples-ch04-spouts/blob/master/src/main/java/twitter/streaming/Topology.java
+ * for the original version.
+ *
+ */
+public class Topology {
+ 
+	public static void main(String[] args) throws InterruptedException {
+		TopologyBuilder builder = new TopologyBuilder();
+		builder.setSpout("tweets-collector", new ApiStreamingSpout(),1);
+		builder.setBolt("hashtag-sumarizer", new TwitterSumarizeHashtags()).
+			shuffleGrouping("tweets-collector"); 
+
+		LocalCluster cluster = new LocalCluster();
+		Config conf = new Config();
+		conf.put("track", args[0]);
+		conf.put("user", args[1]);
+		conf.put("password", args[2]);
+
+		cluster.submitTopology("twitter-test", conf, builder.createTopology());
+	}
 }
