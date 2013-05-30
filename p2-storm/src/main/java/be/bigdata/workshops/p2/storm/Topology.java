07bf563ff3b24f40d688844b5b70a736ce7577a0
Geoffrey De Smet <gds.geoffrey.de.smet@gmail.com>
2013-05-30 21:46:27 +0200

    TwitterSpout

--- p2-storm/src/main/java/be/bigdata/workshops/p2/storm/Topology.java
+++ p2-storm/src/main/java/be/bigdata/workshops/p2/storm/Topology.java
@@ -3,7 +3,6 @@ package be.bigdata.workshops.p2.storm;
 import backtype.storm.Config;
 import backtype.storm.LocalCluster;
 import backtype.storm.generated.StormTopology;
-import backtype.storm.testing.TestWordSpout;
 import backtype.storm.topology.TopologyBuilder;
 import backtype.storm.utils.Utils;
 
@@ -13,16 +12,16 @@ public class Topology {
         System.out.println("Team 3");
         StormTopology topology = buildTopology();
         LocalCluster cluster = new LocalCluster();
-        cluster.submitTopology("team3Tolopogy", new Config(), topology);
-        Utils.sleep(20);
-        cluster.killTopology("team3Tolopogy");
+        cluster.submitTopology("team3Topology", new Config(), topology);
+        Utils.sleep(20000);
+        cluster.killTopology("team3Topology");
         cluster.shutdown();
     }
 
     private static StormTopology buildTopology() {
         TopologyBuilder builder = new TopologyBuilder();
-        builder.setSpout("nonsenseBolt", new TestWordSpout());
-        builder.setBolt("systemOutBolt", new SystemOutBolt()).shuffleGrouping("nonsenseBolt");
+        builder.setSpout("twitterSpout", new TwitterSpout());
+        builder.setBolt("systemOutBolt", new SystemOutBolt()).shuffleGrouping("twitterSpout");
         return builder.createTopology();
     }
 
---
+++ p2-storm/src/main/java/be/bigdata/workshops/p2/storm/TwitterSpout.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2013 JBoss Inc
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package be.bigdata.workshops.p2.storm;
+
+import java.util.Map;
+
+import backtype.storm.spout.SpoutOutputCollector;
+import backtype.storm.task.TopologyContext;
+import backtype.storm.topology.BasicOutputCollector;
+import backtype.storm.topology.OutputFieldsDeclarer;
+import backtype.storm.topology.base.BaseBasicBolt;
+import backtype.storm.topology.base.BaseRichSpout;
+import backtype.storm.tuple.Fields;
+import backtype.storm.tuple.Tuple;
+import backtype.storm.tuple.Values;
+
+public class TwitterSpout extends BaseRichSpout {
+
+    private SpoutOutputCollector spoutOutputCollector;
+
+    public void declareOutputFields(OutputFieldsDeclarer outputFieldsDeclarer) {
+        outputFieldsDeclarer.declare(new Fields("author", "message"));
+    }
+
+    public void open(Map map, TopologyContext topologyContext, SpoutOutputCollector spoutOutputCollector) {
+        this.spoutOutputCollector = spoutOutputCollector;
+    }
+
+    public void nextTuple() {
+        spoutOutputCollector.emit(new Values("Bart", "don't have a cow man"));
+//        spoutOutputCollector.emit(new Values("Homer", "doh"));
+    }
+
+}
